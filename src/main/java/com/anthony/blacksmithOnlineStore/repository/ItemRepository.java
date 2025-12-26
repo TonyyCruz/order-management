00f5f4bfb427dfa0ae4b590b37d30942fd148519
Anthony Cruz <tony_esqueloto@hotmail.com>
2025-12-26 14:25:06 -0300

    feat: adiciona queries customizadas para itesms para evitar reducao de estoques invalidos

--- src/main/java/com/anthony/blacksmithOnlineStore/repository/ItemRepository.java
+++ src/main/java/com/anthony/blacksmithOnlineStore/repository/ItemRepository.java
@@ -5,8 +5,26 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ItemRepository extends JpaRepository<Item, Long>, JpaSpecificationExecutor<Item> {
 
   Page<Item> findByCraftedById(Long blacksmithId, Pageable pageable);
+
+  @Modifying
+  @Query("""
+        UPDATE Item i
+        SET i.stock = i.stock - :qty
+        WHERE i.id = :itemId AND i.stock >= :qty
+    """)
+  int decrementStock(Long productId, int qty);
+
+  @Modifying
+  @Query("""
+        UPDATE Item i
+        SET i.stock = i.stock + :qty
+        WHERE i.id = :itemId
+    """)
+  int incrementStock(Long productId, int qty);
 }
